637d1b4981cba1075565006648cb82c57da34612
mumtaz <mumtaz@mumtazs-MacBook-Pro.local>
2025-01-21 15:48:42 +0530

    search -box created,app.js,app.css,index.js modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React,{ Component } from 'react';
 import { CardList } from './components/card-list/card-list.component'
-
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters :users }));
   }
 
-  render(){
-    return(
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const {  monsters, searchField} =this.state; 
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
+
+    return ( 
       <div className="App">
-        <CardList monsters ={this.state.monsters}/>
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters ={filteredMonsters}/>
       </div>
     );
     
